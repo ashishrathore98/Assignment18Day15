a3bcd1306d3d3bdc4d5a61eb8fb208c2bb5db727
Ashish Rathore <ashrathore98@gmail.com>
2023-07-28 16:02:10 +0530

    first commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using Assignment18Day15;
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Smartphone smartphone = new Smartphone();
+        smartphone.Connect();
+        smartphone.Charge(120);
+        Console.WriteLine($"Smartphone connected: {smartphone.Connect()}");
+        Console.WriteLine($"Smartphone Battery Level: {smartphone.BatteryLevel}");
+        Console.WriteLine(smartphone.Display());
+
+        Console.WriteLine();
+
+        Laptop laptop = new Laptop();
+        laptop.Connect();
+        laptop.Charge(60);
+        Console.WriteLine($"Laptop connected: {laptop.Connect()}");
+        Console.WriteLine($"Laptop Battery Level: {laptop.BatteryLevel}");
+        Console.WriteLine(laptop.Display());
+    }
+}
